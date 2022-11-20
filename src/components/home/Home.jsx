41250cb3965f2e607244cc4f076463fe83298b57
Dan <pintea.dan988@gmail.com>
2022-11-20 01:36:54 +0100

    ...

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css'
-import minimizeButton from "../../assets/minimize_button.png";
 import art from "../../assets/art-1.png";
+import art2 from "../../assets/art-2.png";
+import art3 from "../../assets/art-3.png";
 
 function Home({handleClick}) {
+
+
     return (
         <div className="home-main">
+
             <div className="home-hero-container">
 
                 <div className="home-hero-container-header">
@@ -20,6 +24,12 @@ function Home({handleClick}) {
 
                 <div className="home-hero-container-body">
 
+                    <div className="hero-art-gallery">
+                        <img className={"art_1 hvr-grow"} src={art} alt={"art-1"} style={{width: "320px", height: "500px"}}/>
+                        <img className={"art_2 hvr-grow"} src={art2} alt={"art-2"} style={{width: "320px", height: "500px"}}/>
+                        <img className={"art_3 hvr-grow"} src={art3} alt={"art-3"} style={{width: "320px", height: "500px"}}/>
+                    </div>
+
                     <div className="home-hero-container-body-header">
                         <p>
                             <b>Neubrutalism</b>, or <b>Neobrutalism</b> as some people call it, is a mix of regular
@@ -29,9 +39,7 @@ function Home({handleClick}) {
                     </div>
 
 
-                    <div>
-                        <img className={"art"} src={art} alt={"art-1"} style={{width: "280px", height: "400px"}}/>
-                    </div>
+
 
                 </div>
 
